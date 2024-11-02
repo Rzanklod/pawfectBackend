0c75821637ae6986d43a42b07ab62b23d5a042f3
Rzanklod <malwinaxxx666@gmail.com>
2024-11-03 00:07:38 +0100

    refactored register endpoint code, previously added endpoints are up

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { APP_PORT, API_ROUTE } = require('./config');
-const userRouter = require('./src/routes/users'); 
+const userRouter = require('./src/routes/users');
+const authRouter = require('./src/routes/auth');
+const refreshRouter = require('./src/routes/refresh'); 
+const logoutRouter = require('./src/routes/logout');
 
 const app = express();
 
@@ -8,6 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(`${API_ROUTE}/users`, userRouter);
+app.use(`${API_ROUTE}/auth`, authRouter);
+app.use(`${API_ROUTE}/refresh`, refreshRouter);
+app.use(`${API_ROUTE}/logout`, logoutRouter);
 
 app.listen(APP_PORT, () => {
   console.log(`App listening on port ${APP_PORT}`);
---
+++ src/controllers/registerController.js
@@ -0,0 +1,50 @@
+const bcrypt = require('bcrypt');
+const sql = require('./db');
+
+const handleNewUser = async (req, res) => {
+    try{
+        const username = req.body?.username;
+        const password = req.body?.password;
+        const email = req.body?.email;
+        
+        if(!username || !password || !email){
+            return res.status(400).json({ 'message': 'Username, password and email are required.' });
+        }
+
+        if(
+            (username.length > 24 || password.length > 32 || email.length > 254) 
+            || 
+            (username.length < 3 || password.length < 8)){
+            return res.status(400).json({ 'message': 'Username, password or email has invalid length.' });
+        }
+
+        const user = await sql`
+            SELECT username 
+            FROM users 
+            WHERE username = ${username} OR email = ${email}
+        `;
+
+        if(user){
+            return res.status(409).json({ 'message': 'User with this username or email already exists.' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await sql`
+            INSERT INTO users(
+                username, hashed_password, email
+            )
+            VALUES(
+                ${username}, ${hashedPassword}, ${email}
+            )
+            RETURNING *
+        `;
+        console.log(newUser);
+
+        return res.sendStatus(201);
+    } catch {
+        return res.sendStatus(500);
+    }
+}
+
+module.exports = { handleNewUser };
