5ab432a1df23cdfba46ba45498e62dfc142d9847
wKobacki <woj.kobacki@gmail.com>
2024-11-14 22:28:10 +0100

    bug fix

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,18 +1,17 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const sql = require('./db');
+const sql = require('./db'); 
 const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = require('../../config');
 
 const handleLogin = async (req, res) => {
     try {
         const { username, password } = req.body;
-        
+
         if (!username || !password) {
             console.log('Username or password not provided');
             return res.status(400).json({ message: 'Username and password are required.' });
         }
 
-        // Pobranie użytkownika z bazy
         const foundUser = await sql`
             SELECT username, hashed_password
             FROM users
@@ -24,26 +23,23 @@ const handleLogin = async (req, res) => {
             return res.status(401).json({ message: 'Invalid username or password.' });
         }
 
-        // Porównanie hasła
         const match = await bcrypt.compare(password, foundUser[0].hashed_password);
         if (!match) {
             console.log('Password does not match');
             return res.status(401).json({ message: 'Invalid username or password.' });
         }
 
-        // Generowanie tokenów
         console.log('Generating tokens');
         const accessToken = jwt.sign({ username }, ACCESS_TOKEN_SECRET, { expiresIn: '60s' });
         const refreshToken = jwt.sign({ username }, REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
 
-        // Aktualizacja tokenu odświeżania w bazie
         await sql`
             UPDATE users
             SET refresh_token = ${refreshToken}
             WHERE username = ${username}
         `;
 
-        // Ustawienie ciasteczka z tokenem odświeżania
+        console.log('Setting cookie with refresh token');
         res.cookie('jwt', refreshToken, { 
             secure: true,
             maxAge: 24 * 60 * 60 * 1000,
