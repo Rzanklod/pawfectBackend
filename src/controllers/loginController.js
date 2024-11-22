7c45739381b8955e7f742d6e383f9c44a4b81311
Wegles <kuba2255kuba@gmail.com>
2024-11-22 10:53:00 +0100

    refactor

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,55 +1,61 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const sql = require('./db'); 
-const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = require('../../config');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const sql = require("./db");
+const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = require("../../config");
 
 const handleLogin = async (req, res) => {
-    try {
-        const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-        if (!username || !password) {
-            console.log('Username or password not provided');
-            return res.status(400).json({ message: 'Username and password are required.' });
-        }
+    if (!username || !password) {
+      console.log("Username or password not provided");
+      return res
+        .status(400)
+        .json({ message: "Username and password are required." });
+    }
 
-        const foundUser = await sql`
+    const foundUser = await sql`
             SELECT id, username, hashed_password
             FROM users
             WHERE username = ${username}
         `;
-        
-        if (foundUser.length === 0) {
-            console.log('User not found');
-            return res.status(401).json({ message: 'Invalid username or password.' });
-        }
 
-        const match = await bcrypt.compare(password, foundUser[0].hashed_password);
-        if (!match) {
-            console.log('Password does not match');
-            return res.status(401).json({ message: 'Invalid username or password.' });
-        }
+    if (foundUser.length === 0) {
+      console.log("User not found");
+      return res.status(401).json({ message: "Invalid username or password." });
+    }
 
-        console.log('Generating tokens');
-        const accessToken = jwt.sign({ username }, ACCESS_TOKEN_SECRET, { expiresIn: '60s' });
-        const refreshToken = jwt.sign({ username }, REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
+    const match = await bcrypt.compare(password, foundUser[0].hashed_password);
+    if (!match) {
+      console.log("Password does not match");
+      return res.status(401).json({ message: "Invalid username or password." });
+    }
 
-        await sql`
+    console.log("Generating tokens");
+    const accessToken = jwt.sign({ username }, ACCESS_TOKEN_SECRET, {
+      expiresIn: "600s",
+    });
+    const refreshToken = jwt.sign({ username }, REFRESH_TOKEN_SECRET, {
+      expiresIn: "1d",
+    });
+
+    await sql`
             UPDATE users
             SET refresh_token = ${refreshToken}
             WHERE username = ${username}
         `;
 
-        console.log('Setting cookie with refresh token');
-        res.cookie('jwt', refreshToken, { 
-            secure: true,
-            maxAge: 24 * 60 * 60 * 1000,
-            httpOnly: true 
-        });
-        return res.json({ uid: foundUser[0].id, accessToken });
-    } catch (error) {
-        console.error('Login error:', error);
-        return res.status(500).json({ message: 'Internal Server Error' });
-    }
+    console.log("Setting cookie with refresh token");
+    res.cookie("jwt", refreshToken, {
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
+    return res.json({ uid: foundUser[0].id, accessToken });
+  } catch (error) {
+    console.error("Login error:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 module.exports = { handleLogin };
--- src/controllers/refreshTokenController.js
+++ src/controllers/refreshTokenController.js
@@ -1,37 +1,34 @@
-const jwt = require('jsonwebtoken');
-const sql = require('./db');
-const { REFRESH_TOKEN_SECRET, ACCESS_TOKEN_SECRET } = require('../../config');
+const jwt = require("jsonwebtoken");
+const sql = require("./db");
+const { REFRESH_TOKEN_SECRET, ACCESS_TOKEN_SECRET } = require("../../config");
 
 const handleRefreshToken = async (req, res) => {
-    try{
-        const cookies = req.cookies;
-        if(!cookies?.jwt) return res.sendStatus(401);
-        const refreshToken = cookies.jwt;
-        
-        const foundUser = await sql`
+  try {
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(401);
+    const refreshToken = cookies.jwt;
+
+    const foundUser = await sql`
             SELECT id, username, refresh_token
             FROM users
             WHERE refresh_token = ${refreshToken}
         `;
-        
-        if(!foundUser) return res.sendStatus(403);
-        
-        jwt.verify(
-            refreshToken,
-            REFRESH_TOKEN_SECRET,
-            (err, decoded) => {
-                if(err || foundUser[0].username !== decoded.username) return res.sendStatus(401);
-                const accessToken = jwt.sign(
-                    { "username": decoded.username }, 
-                    ACCESS_TOKEN_SECRET,
-                    { expiresIn: '60s' }
-                );
-                res.json({ uid: foundUser[0].id, accessToken });
-            }
-        )
-    } catch {
-        return res.sendStatus(500);
-    }
-}
+
+    if (!foundUser) return res.sendStatus(403);
+
+    jwt.verify(refreshToken, REFRESH_TOKEN_SECRET, (err, decoded) => {
+      if (err || foundUser[0].username !== decoded.username)
+        return res.sendStatus(401);
+      const accessToken = jwt.sign(
+        { username: decoded.username },
+        ACCESS_TOKEN_SECRET,
+        { expiresIn: "600s" }
+      );
+      res.json({ uid: foundUser[0].id, accessToken });
+    });
+  } catch {
+    return res.sendStatus(500);
+  }
+};
 
 module.exports = { handleRefreshToken };
